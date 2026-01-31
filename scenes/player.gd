e8e7e0145fe2f5de111093b957432ff3c1fe31e6
Norbert W <n.j.wlodarczyk@gmail.com>
2026-01-31 14:19:45 +0100

    Add mask and double jump mask handling

--- scenes/player.gd
+++ scenes/player.gd
@@ -6,11 +6,21 @@ extends CharacterBody2D
 var jump_count := 0
 var max_jump_count := 2
 
+enum Mask {NONE, DOUBLE_JUMP}
+var equipped_mask := Mask.NONE
+
 func _physics_process(delta: float) -> void:
 	apply_gravity(delta)
 	handle_jump_input()
 	handle_horizontal_movement()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("switch_mask_none"):
+		print("Picked mask: NONE")
+		equipped_mask = Mask.NONE
+	elif event.is_action_pressed("switch_mask_double_jump"):
+		print("Picked mask: DOUBLE_JUMP")
+		equipped_mask = Mask.DOUBLE_JUMP
 
 func apply_gravity(delta: float) -> void:
 	if not is_on_floor():
@@ -22,7 +32,7 @@ func handle_jump_input() -> void:
 	if Input.is_action_just_pressed("character_jump"):
 		if is_on_floor():
 			perform_jump()
-		elif jump_count < max_jump_count:
+		elif jump_count < max_jump_count and equipped_mask == Mask.DOUBLE_JUMP:
 			perform_jump()
 
 func perform_jump() -> void:
