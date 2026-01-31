a9cc1a1abaa3dd653c6a751e51b61fedd509f06c
Norbert W <n.j.wlodarczyk@gmail.com>
2026-01-31 23:30:00 +0100

    Add base final boss scene

---
+++ scenes/levels/final_boss.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@onready var camera_2d: Camera2D = $Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	camera_2d.enabled = true
+	camera_2d.make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
