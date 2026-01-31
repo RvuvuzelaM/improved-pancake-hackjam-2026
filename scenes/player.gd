ba5c9a7ad9dd8ea3fe4d3714b193ce5cbc376ebe
swick <maciej.balcewicz@af.agency>
2026-01-31 12:03:26 +0100

    camera following player

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,23 +1,41 @@
 extends CharacterBody2D
 
+const SPEED := 400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const BASE_JUMP_VELOCITY := -550.0
+const EXTRA_JUMP_FORCE := -700.0   # total additional upward force
+const MAX_JUMP_HOLD_TIME := 0.6    # seconds
 
+var jump_hold_time := 0.0
+var is_jumping := false
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		# Reset jump state when grounded
+		is_jumping = false
+		jump_hold_time = 0.0
 
-	# Handle jump.
+	# Jump start
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = BASE_JUMP_VELOCITY
+		is_jumping = true
+		jump_hold_time = 0.0
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Variable jump height
+	if is_jumping and Input.is_action_pressed("ui_accept"):
+		if jump_hold_time < MAX_JUMP_HOLD_TIME:
+			var t := jump_hold_time / MAX_JUMP_HOLD_TIME
+			velocity.y += EXTRA_JUMP_FORCE * delta * (1.0 - t)
+			jump_hold_time += delta
+	else:
+		is_jumping = false
+
+	# Horizontal movement
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
+	if direction != 0.0:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
