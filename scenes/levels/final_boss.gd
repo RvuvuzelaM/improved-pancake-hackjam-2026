40d5280ba9ca6e01c660bc98d90ac5e065b55ffe
Łukasz <lukaszczech98@gmail.com>
2026-02-01 01:15:13 +0100

    Final boss update, and adding platform as comp

--- scenes/levels/final_boss.gd
+++ scenes/levels/final_boss.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var camera_2d: Camera2D = $Camera2D
+@onready var boss: Node2D = $Boss
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_timer_timeout() -> void:
+	camera_2d.position.y -= 1
+	boss.global_position.y -= 1
+	print(boss.global_position.y)
